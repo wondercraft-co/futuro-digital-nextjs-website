fd97e48927557155e9d3e90095280e5c0faae4b7
Juan Pablo Solano <hello@wondercraft.co>
2024-10-01 22:02:56 -0500

    og route

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,47 +1,41 @@
-import { ImageResponse } from 'next/og';
-// App router includes @vercel/og.
-// No need to install it.
- 
+import { ImageResponse } from "next/og";
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const username = searchParams.get('username');
-  if (!username) {
-    return new ImageResponse(<>Visit with &quot;?username=vercel&quot;</>, {
-      width: 1200,
-      height: 630,
-    });
-  }
- 
+  const title = searchParams.get("title");
+
   return new ImageResponse(
     (
       <div
         style={{
-          display: 'flex',
-          fontSize: 60,
-          color: 'black',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          paddingTop: 50,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
         }}
       >
         <img
-          width="256"
-          height="256"
-          src={`https://github.com/${username}.png`}
-          style={{
-            borderRadius: 128,
-          }}
+          width="1200"
+          height="630"
+          src={`https://futurodigital.wondercraft.co/_next/image?url=%2Fog-bg.jpg&w=3840&q=75`}
         />
-        <p>github.com/{username}</p>
+        {title ? (
+          <h1
+            style={{
+              position: "absolute",
+              top: 320,
+              left: 74,
+              width: 680,
+              color: "white",
+              fontSize: "48px",
+              lineHeight: "48px",
+            }}
+          >
+            Impulsando a jóvenes para transformar su futuro con tecnología
+          </h1>
+        ) : null}
       </div>
     ),
     {
       width: 1200,
       height: 630,
-    },
+    }
   );
 }
